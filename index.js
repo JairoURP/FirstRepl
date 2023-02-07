64a91c3ab734b0d4ba74575e538175c39a2112bf
Jairo Melendez <113256108+JairoURP@users.noreply.github.com>
2023-02-07 03:36:31 +0000

    Add New Product Product class was added.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //console.log("Hello world")
 import {User} from './User.js'
+import {Product} from './Product.js'
 
 const ryan = new User('Ryan', 'Ray')
 console.log(ryan)
+
+console.log(new Product('laptop', 3000))
+
+
