9c443285b6328ade89251f472fb1d0f102798eb8
Jairo Melendez <>
2023-02-07 03:29:37 +0000

    Initial commit

---
+++ User.js
@@ -0,0 +1,8 @@
+export class User {
+  constructor(name, lastName) {
+    this.name = name
+    this.lastName = lastName
+  }
+}
+//Para crear un package.json
+//Usar comando en terminal: npm init -y
---
+++ index.js
@@ -0,0 +1,5 @@
+//console.log("Hello world")
+import {User} from './User.js'
+
+const ryan = new User('Ryan', 'Ray')
+console.log(ryan)
